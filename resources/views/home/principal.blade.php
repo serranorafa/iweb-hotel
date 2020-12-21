5754f19ff6c34f956c08c49550eaf9acf8143f3f
Rafael Serrano Aracil <rsa26@alu.ua.es>
2020-12-21 15:35:18 +0100

    Primer commit

---
+++ resources/views/home/principal.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="UTF-8">
+ <title>Mi flamante proyecto con Lrvl</title>
+ </head>
+ <body>
+ <header>
+ <h1>Bienvenido, {{$nombre}}, a la web del proyecto</h1>
+ </header>
+ <main>
+ <h2>En esta web podrás hacer ....</h2>
+ </main>
+ <footer>&copy; 2020 iw.ua.es</footer>
+ </body>
+</html>

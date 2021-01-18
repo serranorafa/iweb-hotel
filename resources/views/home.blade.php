c2e53fadd0c42c8bfd4e3628b3e84704af432677
Rafael Serrano Aracil <rsa26@alu.ua.es>
2021-01-18 16:03:01 +0100

    Imagenes del carrousel añadidas en public/img/home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,24 +10,24 @@
     <li data-target="#carouselHome" data-slide-to="1" class=""></li>
     <li data-target="#carouselHome" data-slide-to="2" class=""></li>
   </ol>
-  <div class="carousel-inner">
+  <div class="carousel-inner" style="overflow:hidden" >
     <div class="carousel-item active">
-        <img src="https://via.placeholder.com/1000x600" />
+        <img src="{{ url('/img/home/home-1.jpeg') }}" style="height:600px; width:1000px;" />
     </div>
     <div class="carousel-item">
-    <img src="https://via.placeholder.com/1000x600"/>
+      <img src="{{ url('/img/home/home-2.jpeg') }}" style="height:600px; width:1000px;" />
     </div>
     <div class="carousel-item">
-    <img src="https://via.placeholder.com/1000x600"/>
+      <img src="{{ url('/img/home/home-3.jpeg') }}" style="height:600px; width:1000px;" />
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselHome" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
+    <span class="sr-only"></span>
   </a>
   <a class="carousel-control-next" href="#carouselHome" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
+    <span class="sr-only"></span>
   </a>
 </div>
         </div>

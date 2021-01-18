e26ed54123c06a20899a70af2c5fd7b4ae37f193
Rafael Serrano Aracil <rsa26@alu.ua.es>
2021-01-18 21:57:21 +0100

    Añadidas imagenes para la galeria

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="background-color: cyan;">
     <h1 style="text-align: center">{{ __('Fotos') }}</h1>
-
+    <img src="{{ asset('/img/room/room-1.webp') }}" />
 </div>
 @endsection

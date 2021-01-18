840bb9c8d49626c3656c89654e3f43e431555831
Miguel Angel Garcia Pico <mgp143@alu.ua.es>
2021-01-18 11:28:07 +0100

    Implementados metodos CRUD para la entidad User

--- app/User.php
+++ app/User.php
@@ -14,6 +14,60 @@ class User extends Authenticatable
 
     public const admin = "WEBMASTER";
 
+    public const recepcionista = "RECEPCIONISTA";
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function getApellidos() {
+        return $this->apellidos;
+    }
+
+    public function setApellidos($apellidos) {
+        $this->apellidos = $apellidos;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    public function getTelefono() {
+        return $this->telefono;
+    }
+
+    public function setTelefono($telefono) {
+        $this->telefono = $telefono;
+    }
+
+    public function getRol() {
+        return $this->rol;
+    }
+
+    public function setRol($rol) {
+        $this->rol = $rol;
+    }
+
     public function getReservas() {
         return $this->hasMany('App\Reserva');
     }

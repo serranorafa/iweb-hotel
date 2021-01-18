1cf04767b9d8b0b754a384257f5f6b21d69b6aec
Jose Francisco Vera Girona <byyiro@hotmail.com>
2021-01-18 03:32:46 +0100

    Vista gestión servicios

---
+++ resources/views/servicios/details.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+        <div class="card">
+            <h1 class="card-head" style="text-align: center">{{$servicio->nombre}}</h1>
+                <div class="row card-body" style="text-align: center">
+                    <div class="col-md-3 col-12" style="text-align: left">
+                        <h4 style="font-weight: 900">
+                            ID:
+                        </h4>
+                    </div>
+                    <div class="col-md-3 col-12" style="text-align: left">
+                            {{$servicio->id}}
+                    </div>
+                    <div class="col-md-6 col-12"></div>
+                    <div class="col-md-3 col-12" style="text-align: left">
+                        <h4 style="font-weight: 900">
+                            Nombre:
+                        </h4>
+                    </div>
+                    <div class="col-md-3 col-12" style="text-align: left">
+                            {{$servicio->nombre}}
+                    </div>
+                    <div class="col-md-3 col-12" style="text-align: left">
+                        <h4 style="font-weight: 900">
+                            Descripción:
+                        </h4>
+                    </div>
+                    <div class="col-md-3 col-12" style="text-align: left">
+                            {{$servicio->descripcion}}
+                    </div>
+                    <div class="col-md-3 col-12" style="text-align: left">
+                        <h4 style="font-weight: 900">
+                            Tarifa diaria:
+                        </h4>
+                    </div>
+                    <div class="col-md-3 col-12" style="text-align: left">
+                        {{$servicio->tarifa}}€
+                    </div>
+                    <div class="col-12" style="text-align: center">
+                        <a class="btn btn-danger" style="text-align: center">
+                            {{ __('Eliminar') }}
+                        </a>
+                        <a href="/servicios/{{$servicio->id}}/edit" class="btn btn-secondary" style="text-align: center">
+                            {{ __('Editar') }}
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <br />  
+            <div style="text-align: center">
+                <a href="/servicios" class="btn btn-secondary">
+                    {{ __('Volver al listado') }}
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/servicios/list.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <h1 style="text-align: center">{{ __('Servicios') }}</h1>
+            <div class="card" style="text-align: left">
+                <div class="card-body">
+                <h4 style="text-align: center">{{ __('Búsqueda') }}</h4>
+                    <form method="POST">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="id" class="col-lg-2 col-12 col-form-label text-md-right">{{ __('ID') }}</label>
+
+                            <div class="col-lg-4 col-12" style="padding-right: 10%">
+                                <input id="id" class="form-control" type="text" name="id" autocomplete="id" autofocus>
+                            </div>
+                            <label for="nombre" class="col-lg-2 col-12 col-form-label text-md-right">{{ __('Nombre') }}</label>
+                            <div class="col-lg-4 col-12" style="padding-right: 10%">
+                                <input id="nombre" class="form-control" type="text" name="nombre" autocomplete="nombre" autofocus>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="form-group row">
+                            <label for="tarifa" class="col-lg-2 col-12 col-form-label text-md-right">{{ __('Tarifa') }}</label>
+                            <div class="col-lg-2 col-12">
+                                <select id="comparacion" name="comparacion" class="form-control" autofocus>
+                                    <option value="<=" selected>Hasta</option>
+                                    <option value="==">Igual a</option>
+                                    <option value=">">Mayor a</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-2 col-12" style="padding-right: 10%">
+                                <input id="tarifa" class="form-control" type="number" name="tarifa" autocomplete="tarifa" autofocus>
+                            </div>
+                        </div>
+                        <br>
+                        <br>
+                        <div class="form-group row" style="text-align: center">
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-secondary" style="text-align: center"> 
+                                    {{ __('Aplicar filtros') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <br>
+            <div style="text-align: center">
+               <a href="/servicios/create" class="btn btn-secondary">+ Nuevo servicio</a>
+            </div>
+            <br>
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Tarifa diaria</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($servicios as $servicio)
+                    <tr>
+                        <th scope="row">{{$servicio->id}}</th>
+                        <td>{{$servicio->nombre}}</td>
+                        <td>{{$servicio->tarifa}}€</td>
+                        <td><a href="/servicios/{{$servicio->id}}">Detalles</a></td>
+                        <td><a href="/servicios/{{$servicio->id}}/edit">Editar</a></td>
+                        <td><a href="#">Eliminar</a></td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <div style="align-items: center">
+            <div style="width:max-content; margin:auto">
+            {{$servicios->links()}}
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

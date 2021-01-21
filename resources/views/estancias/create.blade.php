06abc7ff53ce8a2b03505d4f533538f72bc499fc
Rafael Serrano Aracil <rsa26@alu.ua.es>
2021-01-21 01:15:09 +0100

    Validacion crear/editar estancias

--- resources/views/estancias/create.blade.php
+++ resources/views/estancias/create.blade.php
@@ -13,7 +13,7 @@
                             <label for="numero" class="col-md-4 col-form-label text-md-right">{{ __('Número') }}</label>
 
                             <div class="col-md-6">
-                            <input id="numero" type="number" class="form-control @error('numero') is-invalid @enderror" name="numero" required autocomplete="numero" autofocus>
+                            <input id="numero" type="number" class="form-control @error('numero') is-invalid @enderror" name="numero" value="{{ old('numero') }}" required autocomplete="numero" autofocus>
                                 @error('numero')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -25,7 +25,7 @@
                         <div class="form-group row">
                             <label for="tipo" class="col-md-4 col-form-label text-md-right">{{ __('Tipo') }}</label>
                             <div class="col-md-6">
-                                <select id="tipo" name="tipo" autofocus onchange="cambiarVisibilidad()">
+                                <select id="tipo" class="form-control" name="tipo" value="{{ old('tipo') }}" autofocus onchange="cambiarVisibilidad()">
                                     <option value="HABITACION">Habitación</option>
                                     <option value="SALA">Sala</option>
                                 </select>
@@ -35,7 +35,7 @@
                         <div class="form-group row">
                             <label for="planta" class="col-md-4 col-form-label text-md-right">{{ __('Planta') }}</label>
                             <div class="col-md-6">
-                                <input id="planta" type="number" class="form-control @error('planta') is-invalid @enderror" name="planta" required autocomplete="planta" autofocus>
+                                <input id="planta" type="number" class="form-control @error('planta') is-invalid @enderror" name="planta" value="{{ old('planta') }}" required autocomplete="planta" autofocus>
                                 @error('planta')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -47,7 +47,7 @@
                         <div class="form-group row habitacion" style="visibility:visible">
                             <label for="plazas" class="col-md-4 col-form-label text-md-right">{{ __('Plazas') }}</label>
                             <div class="col-md-6">
-                                <input id="plazas" type="number" class="form-control @error('plazas') is-invalid @enderror" name="plazas" autocomplete="plazas" autofocus>
+                                <input id="plazas" type="number" class="form-control @error('plazas') is-invalid @enderror" name="plazas" value="{{ old('plazas') }}" autocomplete="plazas" autofocus>
                                 @error('plazas')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -59,7 +59,12 @@
                         <div class="form-group row habitacion" style="visibility:visible">
                             <label for="vistas" class="col-md-4 col-form-label text-md-right">{{ __('Vistas') }}</label>
                             <div class="col-md-6">
-                                <input id="vistas" type="text" class="form-control @error('vistas') is-invalid @enderror" name="vistas" autocomplete="vistas" autofocus>
+                                <select id="vistas" class="form-control" name="vistas" value="{{ old('vistas') }}" autofocus>
+                                    <option value="Vistas a piscina">Vistas a piscina</option>
+                                    <option value="Vistas al jardin">Vistas al jardin</option>
+                                    <option value="Vistas al mar">Vistas al mar</option>
+                                    <option value="Vistas al aparcamiento">Vistas al aparcamiento</option>
+                                </select>
                                 @error('vistas')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -71,7 +76,7 @@
                         <div class="form-group row sala" style="visibility:hidden">
                             <label for="aforo" class="col-md-4 col-form-label text-md-right">{{ __('Aforo') }}</label>
                             <div class="col-md-6">
-                                <input id="aforo" type="number" class="form-control @error('aforo') is-invalid @enderror" name="aforo" autocomplete="aforo" autofocus>
+                                <input id="aforo" type="number" class="form-control @error('aforo') is-invalid @enderror" name="aforo" autocomplete="aforo" value="{{ old('aforo') }}" autofocus>
                                 @error('aforo')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -83,7 +88,7 @@
                         <div class="form-group row">
                             <label for="descripcion" class="col-md-4 col-form-label text-md-right">{{ __('Descripción') }}</label>
                             <div class="col-md-6">
-                                <textarea id="descripcion" class="form-control @error('descripcion') is-invalid @enderror" name="descripcion" required autocomplete="descripcion" autofocus></textarea>
+                                <textarea id="descripcion" class="form-control @error('descripcion') is-invalid @enderror" name="descripcion" value="{{ old('descripcion') }}" required autocomplete="descripcion" autofocus></textarea>
                                 @error('descripcion')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -96,7 +101,7 @@
                             <label for="tarifa_base" class="col-md-4 col-form-label text-md-right">{{ __('Tarifa base') }}</label>
 
                             <div class="col-md-6">
-                                <input id="tarifa_base" type="number" class="form-control @error('tarifa_base') is-invalid @enderror" name="tarifa_base" required autocomplete="tarifa_base" autofocus>
+                                <input id="tarifa_base" type="number" class="form-control @error('tarifa_base') is-invalid @enderror" name="tarifa_base" value="{{ old('tarifa_base') }}" required autocomplete="tarifa_base" autofocus>
                                 @error('tarifa_base')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -109,12 +114,12 @@
                             <label for="foto" class="col-md-4 col-form-label text-md-right">{{ __('Imagen') }}</label>
 
                             <div class="col-md-6">
-                                <input id="foto" type="file" accept="image/jpeg, image/jpg, image/png" class="form-control @error('tarifa_base') is-invalid @enderror" name="fotos[]" required autocomplete="foto" autofocus multiple>
-                                @error('foto')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input id="foto" type="file" accept="image/jpeg, image/jpg, image/png" class="form-control" name="fotos[]" required autocomplete="foto" autofocus multiple>
+                                @if(count($errors)>0)
+                                    <div class="alert alert-danger">
+                                        {{ $errors->first() }}
+                                    </div>
+                                @endif
                             </div>
                         </div>
                         <br>

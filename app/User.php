f40166d4f75ae761cd95ebf083590080732f0cfa
Miguel Angel Garcia Pico <mgp143@alu.ua.es>
2021-01-22 15:17:37 +0100

    Commit

--- app/User.php
+++ app/User.php
@@ -5,10 +5,16 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\MailResetPasswordNotification;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new MailResetPasswordNotification($token));
+    }
 
     public const user = "CLIENTE";
 
